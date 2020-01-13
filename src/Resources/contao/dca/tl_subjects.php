7bdb12b397c7e062adfd2dc295a0d5034000878c
Rolf Staege <rolf.staege@lumturo.net>
2020-01-13 09:08:15 +0100

    3 Bilder für Fachgebiete hinterlegt

--- src/Resources/contao/dca/tl_subjects.php
+++ src/Resources/contao/dca/tl_subjects.php
@@ -10,70 +10,56 @@ $table = 'tl_subjects';
 /**
  * Table tl_jobs
  */
-$GLOBALS['TL_DCA'][$table] = array
-(
+$GLOBALS['TL_DCA'][$table] = array(
     // Config
-    'config' => array
-    (
+    'config' => array(
         'dataContainer'               => 'Table',
-        'sql' => array
-        (
-            'keys' => array
-            (
+        'sql' => array(
+            'keys' => array(
                 'id' => 'primary'
             )
         )
     ),
 
     // List
-    'list' => array
-    (
-        'sorting' => array
-        (
+    'list' => array(
+        'sorting' => array(
             'mode'                    => 2,
-            'flag'					  => 1,
+            'flag'                      => 1,
             'fields'                  => array('title'),
             'format'                  => '%s',
             'panelLayout'             => 'filter;sort,search,limit'
         ),
-        'label' => array
-        (
+        'label' => array(
             'fields'                  => array('title'),
             'format'                  => '%s'
         ),
-        'global_operations' => array
-        (
-            'all' => array
-            (
+        'global_operations' => array(
+            'all' => array(
                 'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
                 'href'                => 'act=select',
                 'class'               => 'header_edit_all',
                 'attributes'          => 'onclick="Backend.getScrollOffset();"'
             )
         ),
-        'operations' => array
-        (
-            'edit' => array
-            (
+        'operations' => array(
+            'edit' => array(
                 'label'               => &$GLOBALS['TL_LANG'][$table]['edit'],
                 'href'                => 'act=edit',
                 'icon'                => 'edit.gif'
             ),
-            'copy' => array
-            (
+            'copy' => array(
                 'label'               => &$GLOBALS['TL_LANG'][$table]['copy'],
                 'href'                => 'act=copy',
                 'icon'                => 'copy.gif'
             ),
-            'delete' => array
-            (
+            'delete' => array(
                 'label'               => &$GLOBALS['TL_LANG'][$table]['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.gif',
                 'attributes'          => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"'
             ),
-            'show' => array
-            (
+            'show' => array(
                 'label'               => &$GLOBALS['TL_LANG'][$table]['show'],
                 'href'                => 'act=show',
                 'icon'                => 'show.gif'
@@ -82,35 +68,73 @@ $GLOBALS['TL_DCA'][$table] = array
     ),
 
     // Palettes
-    'palettes' => array
-    (
-        'default'                     => '{lbl_general},title;'
+    'palettes' => array(
+        'default'                     => '{lbl_general},title;{images_legend},img1,img1Alt,img2,img2Alt,img3,img3Alt'
     ),
 
     // Subpalettes
-    'subpalettes' => array
-    (
-    ),
+    'subpalettes' => array(),
 
     // Fields
-    'fields' => array
-    (
-        'id' => array
-        (
+    'fields' => array(
+        'id' => array(
             'sql'                     => "int(10) unsigned NOT NULL auto_increment"
         ),
-        'tstamp' => array
-        (
+        'tstamp' => array(
             'sql'                     => "int(10) unsigned NOT NULL default '0'"
         ),
-        'title' => array
-        (
+        'title' => array(
             'label'                   => &$GLOBALS['TL_LANG'][$table]['title'],
             'exclude'                 => true,
             'search'                  => true,
             'inputType'               => 'text',
-            'eval'                    => array('mandatory'=>true, 'maxlength'=>255),
+            'eval'                    => array('mandatory' => true, 'maxlength' => 255),
             'sql'                     => "varchar(255) NOT NULL default ''"
-        )
+        ),
+        'img1' => array(
+            'label'                   => &$GLOBALS['TL_LANG'][$table]['img1'],
+            'exclude'                 => true,
+            'inputType'               => 'fileTree',
+            'eval'                    => array('filesOnly' => true, 'fieldType' => 'radio', 'mandatory' => false, 'tl_class' => 'clr'),
+            'sql'                     => "binary(16) NULL"
+        ),
+        'img1Alt' => array(
+            'label'                   => &$GLOBALS['TL_LANG'][$table]['alt'],
+            'exclude'                 => true,
+            'search'                  => true,
+            'inputType'               => 'text',
+            'eval'                    => array('maxlength' => 255, 'rgxp' => 'alnum'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'img2' => array(
+            'label'                   => &$GLOBALS['TL_LANG'][$table]['img2'],
+            'exclude'                 => true,
+            'inputType'               => 'fileTree',
+            'eval'                    => array('filesOnly' => true, 'fieldType' => 'radio', 'mandatory' => false, 'tl_class' => 'clr'),
+            'sql'                     => "binary(16) NULL"
+        ),
+        'img2Alt' => array(
+            'label'                   => &$GLOBALS['TL_LANG'][$table]['alt'],
+            'exclude'                 => true,
+            'search'                  => true,
+            'inputType'               => 'text',
+            'eval'                    => array('maxlength' => 255, 'rgxp' => 'alnum'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'img3' => array(
+            'label'                   => &$GLOBALS['TL_LANG'][$table]['img3'],
+            'exclude'                 => true,
+            'inputType'               => 'fileTree',
+            'eval'                    => array('filesOnly' => true, 'fieldType' => 'radio', 'mandatory' => false, 'tl_class' => 'clr'),
+            'sql'                     => "binary(16) NULL"
+        ),
+        'img3Alt' => array(
+            'label'                   => &$GLOBALS['TL_LANG'][$table]['alt'],
+            'exclude'                 => true,
+            'search'                  => true,
+            'inputType'               => 'text',
+            'eval'                    => array('maxlength' => 255, 'rgxp' => 'alnum'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
     )
 );
