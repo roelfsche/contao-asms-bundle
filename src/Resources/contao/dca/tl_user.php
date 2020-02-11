5c953c287337e263fb1a30e9a80d9c679292cffb
Rolf Staege <rolf.staege@lumturo.net>
2020-02-11 10:29:30 +0100

    bugfixing Gruppenrechte

--- src/Resources/contao/dca/tl_user.php
+++ src/Resources/contao/dca/tl_user.php
@@ -23,8 +23,10 @@ $GLOBALS['TL_DCA']['tl_user']['fields']['job_offer_access'] = array
 /**
  * Extend default palette
  */
+$GLOBALS['TL_DCA']['tl_user']['palettes']['default'] = str_replace('email;', 'email;{job_offer_access_legend},job_offer_access;', $GLOBALS['TL_DCA']['tl_user']['palettes']['extend']);
 $GLOBALS['TL_DCA']['tl_user']['palettes']['extend'] = str_replace('email;', 'email;{job_offer_access_legend},job_offer_access;', $GLOBALS['TL_DCA']['tl_user']['palettes']['extend']);
 $GLOBALS['TL_DCA']['tl_user']['palettes']['custom'] = str_replace('email;', 'email;{job_offer_access_legend},job_offer_access;', $GLOBALS['TL_DCA']['tl_user']['palettes']['custom']);
+$GLOBALS['TL_DCA']['tl_user']['palettes']['group'] = str_replace('email;', 'email;{job_offer_access_legend},job_offer_access;', $GLOBALS['TL_DCA']['tl_user']['palettes']['custom']);
 
 
 class tl_asms_user extends Backend
