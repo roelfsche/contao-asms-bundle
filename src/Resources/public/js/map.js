8088e953c06e17607b78be3749093d250b8f3290
mb@click-solutions.de <mb@click-solutions.de>
2020-02-20 11:01:07 +0100

    fix: zoomlevel

--- src/Resources/public/js/map.js
+++ src/Resources/public/js/map.js
@@ -16,8 +16,8 @@ $(function () {
         $filterButton = $('.js-search-button');
 
     var map = L.map('jobmap', {
-        minZoom: 3,
-        maxZoom: 12
+        minZoom: 12,
+        maxZoom: 3
     }).setView([51.133481, 10.018343], 6);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
